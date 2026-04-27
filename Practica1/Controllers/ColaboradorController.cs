7464ddf3089dedda8965fd9ff3c4815c95db2ae8
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-04-27 12:36:41 -0500

    feat(Colaborador): implementar vistas de edición, eliminación y detalle

--- Practica1/Controllers/ColaboradorController.cs
+++ Practica1/Controllers/ColaboradorController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NuGet.DependencyResolver;
 using Practica1.Data.AccesoDatos;
 using Practica1.Models.Entidad;
 
@@ -22,33 +23,92 @@ namespace Practica1.Controllers
 
         [HttpGet]
         public IActionResult Create()
-        {  
+        {
             ViewBag.Empresas = _daEmpresa.GetEmpresas();
             return View();
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Create(Colaborador Entidad)
         {
-           
-            if (!ModelState.IsValid)
-                {   
-                    ViewBag.Empresas = _daEmpresa.GetEmpresas();
-                    return View(Entidad);
-                }
 
-            var id = _daColaborador.InsetarColaborador(Entidad);
-            
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Empresas = _daEmpresa.GetEmpresas();
+                return View(Entidad);
+            }
+
+            var id = _daColaborador.InsertarColaborador(Entidad);
+
             if (id>0)
             {
                 return RedirectToAction("ListadoColaborador");
             } else
             {
-                ModelState.AddModelError("", "Error al guardar colaborador");
                 ViewBag.Empresas = _daEmpresa.GetEmpresas();
                 return View(Entidad);
             }
         }
 
+        //GET Edit
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Empresas = _daEmpresa.GetEmpresas();
+
+            var model = _daColaborador.GetIdColaborador(id);
+
+            if (model == null) return NotFound();
+
+            return View(model);
+        }
+
+        //POST Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+       public IActionResult Edit(Colaborador entidad)
+        {
+            if(!ModelState.IsValid)
+            {
+                ViewBag.Empresas = _daEmpresa.GetEmpresas();
+                return View(entidad);
+            }
+
+            entidad.FechaModificacion = DateTime.Now;
+
+            var resultado = _daColaborador.UpdateColaborador(entidad);
+            
+            if(!resultado) return NotFound();
+
+            return RedirectToAction("ListadoColaborador");
+         }
+
+        public IActionResult Delete(int id)
+        {
+            var model = _daColaborador.GetIdColaborador(id);
+
+            if(model == null) return NotFound();
+
+            return View(model);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var resultado = _daColaborador.DeleteColaborador(id);
+            if(!resultado) return NotFound();
+
+            return RedirectToAction("ListadoColaborador");
+        }
+
+        public IActionResult Details(int id)
+        {
+            var model = _daColaborador.GetIdColaborador(id);
+
+            if(model==null) return NotFound();
+
+            return View(model);
+        }
     }
 }
--- Practica1/Data/AccesoDatos/DAColaborador.cs
+++ Practica1/Data/AccesoDatos/DAColaborador.cs
@@ -1,31 +1,74 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Razor.Language.Intermediate;
+using Microsoft.EntityFrameworkCore;
 using Practica1.Models.Entidad;
 
 namespace Practica1.Data.AccesoDatos
 {
     public class DAColaborador
     {
-        public IEnumerable<Colaborador> GetColaboradores() {
+        private readonly ApplicationDbContext _context;
 
-            using (var db = new ApplicationDbContext())
-            {
-                return db.Colaboradores
+        public DAColaborador(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Colaborador> GetColaboradores() {
+                return _context.Colaboradores
                     .Include(c => c.Empresa)
                     .AsNoTracking()
-                    .ToList();
-            }
+                    .ToList(); 
         }
 
-        public int InsetarColaborador(Colaborador Entidad)
-        {   
-                if (Entidad == null) return 0;
+        public int InsertarColaborador(Colaborador Entidad)
+        {
+            if (Entidad == null) return 0;
 
-                using (var db = new ApplicationDbContext()) { 
-                    db.Colaboradores.Add(Entidad);
-                    db.SaveChanges();
-                    return Entidad.IdColaborador;
-                }
-            }
+                _context.Colaboradores.Add(Entidad);
+                _context.SaveChanges();
+                return Entidad.IdColaborador;
+            
         }
+        
+
+    public Colaborador GetIdColaborador(int id)
+        {
+            return _context.Colaboradores
+                    .Include(c => c.Empresa)
+                    .FirstOrDefault(c => c.IdColaborador == id);  
+        }
+
+        public bool UpdateColaborador(Colaborador entidad)
+        {
+            var existente = _context.Colaboradores
+                .FirstOrDefault(c => c.IdColaborador == entidad.IdColaborador);
+
+            if(existente == null) return false;
+
+            existente.NombColaborador = entidad.NombColaborador;
+            existente.ApeColaborador = entidad.ApeColaborador;
+            existente.DNI = entidad.DNI;
+            existente.Sexo = entidad.Sexo;
+            existente.Direccion = entidad.Direccion;
+            existente.IdEmpresa = entidad.IdEmpresa;
+            existente.FechaModificacion = entidad.FechaModificacion;
+
+            return _context.SaveChanges() > 0;
+        }
+
+        public bool DeleteColaborador(int id)
+        {
+            var existente = _context.Colaboradores
+                .FirstOrDefault(c => c.IdColaborador == id);    
+
+            if(existente == null ) return false;
+
+            _context.Colaboradores.Remove(existente);
+            _context.SaveChanges();
+
+            return true;
+        }
+
     }
+}
 
